7da9e6419b74ed8e7e784ba710a2384629b51433
Jeremy J. Lee <doctorslow@gmail.com>
2018-04-21 01:06:43 -0700

    updated ReadMe file

--- server.js
+++ server.js
@@ -20,3 +20,26 @@ var path = require("path");
 
 // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 //    * The modal should display both the name and picture of the closest match.
+
+
+###
+FriendFinder App
+
+
+// ### Hosting on Heroku
+
+// Now that we have a backend to our applications, we use Heroku for hosting. Please note that while **Heroku is free**, it will request credit card information if you have more than 5 applications at a time or are adding a database. 
+
+// Please see [Heroku’s Account Verification Information](https://devcenter.heroku.com/articles/account-verification) for more details. 
+
+// - - -
+
+// ### Create a README.md
+
+// Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
+
+// * [About READMEs](https://help.github.com/articles/about-readmes/)
+
+// * [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+
+// - - -
